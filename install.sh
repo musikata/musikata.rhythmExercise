47ae587b18453df4f257d0c0f469823615536cee
drKraken <kuchuklehjs@gmail.com>
2015-01-31 13:24:34 +0200

    Initialize travis bower and install script

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!bin/bash
+flag=$1
+base_dir="./lib"
+
+function get_latest {
+
+	if [ -e "$(which wget)" ]; then
+		echo -e "\033[0;35mcopying phaser.min.js => to $base_dir/phaser.js\033[0m"
+		wget -O $base_dir/phaser.js https://raw.githubusercontent.com/photonstorm/phaser/master/build/phaser.min.js ;
+		echo -e "\033[0;35mcopying phaser.min.js => to $base_dir/jquery.js\033[0m"
+		wget -O $base_dir/jquery.js http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js
+	else
+		echo -e "\033[1;30m(error) \033[0;31mwget not installed\033[0m" 
+	fi
+}
+
+function install_npm {
+	if [ -e "$(which npm)" ]; then
+		echo "Installing dependencies ... "
+		npm install
+	fi
+}
+
+echo -e "\033[0;35minstalling latest libraries ...\033[0m"
+get_latest
+install_npm
+bower install
