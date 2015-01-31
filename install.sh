c25464ca7ed11c63546fcf9656942cdbe06ba50e
drKraken <kuchuklehjs@gmail.com>
2015-01-31 22:54:23 +0200

    change folders structure

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!bin/bash
-base_dir="./lib"
+base_dir="./src/lib"
 
 function get_latest {
 
 	if [ -e "$(which wget)" ]; then
 		echo -e "\033[0;35mcopying phaser.min.js => to $base_dir/phaser.js\033[0m"
-		wget -O $base_dir/phaser.js https://raw.githubusercontent.com/photonstorm/phaser/master/build/phaser.min.js ;
+		wget -O $base_dir/phaser.min.js https://raw.githubusercontent.com/photonstorm/phaser/master/build/phaser.min.js ;
 		echo -e "\033[0;35mcopying phaser.min.js => to $base_dir/jquery.js\033[0m"
-		wget -O $base_dir/jquery.js http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js
+		wget -O $base_dir/jquery.min.js http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js
 	else
 		echo -e "\033[1;30m(error) \033[0;31mwget not installed\033[0m" 
 	fi
