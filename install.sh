23cefe2f2a261acc3cd570383064202773db43e5
drKraken <kuchuklehjs@gmail.com>
2015-02-02 22:45:25 +0200

    minor fix and addin installing wget

--- install.sh
+++ install.sh
@@ -9,15 +9,30 @@ function get_latest {
 		echo -e "\033[0;35mcopying phaser.min.js => to $base_dir/jquery.js\033[0m"
 		wget -O $base_dir/jquery.min.js http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js
 	else
-		echo -e "\033[1;30m(error) \033[0;31mwget not installed\033[0m" 
+		echo -e "\033[1;30m(error) \033[0;31mwget not installed\033[0m";
+		installing_wget 
 	fi
 }
 
+function installing_wget {
+
+	echo "Install wget?"
+	read -p "   -> " install;
+	case $install in  
+		yes|Yes|Y ) sudo apt-get install wget ;; 
+		no|No|N )  ;; 
+		*) echo -e "\033[1;30m(error) \033[0;31muncorrect type.\033[0m" ;; 
+	esac
+
+}
+
 function install_npm {
+
 	if [ -e "$(which npm)" ]; then
 		echo "Installing dependencies ... "
 		npm install
 	fi
+	
 }
 
 echo -e "\033[0;35minstalling latest libraries ...\033[0m"
