1422bc90bf6e7e8e2d59052ffdcff85cb40d0c55
adorsk <adorsk@gmail.com>
2015-02-10 15:28:57 -0500

    acceleration detection

--- app/js/game.js
+++ app/js/game.js
@@ -1,99 +1,93 @@
 Phaser = require('Phaser');
-metronome = require('./metronome');
+var metronome = require('./metronome');
 
 console.log('yo');
 
-window.addEventListener("load", function(){ metronome.play()});
-
-var sprite1;
-var sprite2;
-var cursors;
-
 var game = new Phaser.Game(400, 400, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
-var target = new Phaser.Point();
-var springTop;
-var springBall;
-var isMoving = false;
-var dropTop = 120;
-var throttleMoving = false;
-var nextMoveTime = -1;
-var moveDelay = 100;
+var ptrMotionState;
+var oldSign = 1;
+var oldTime = 0;
+window.timeThresh = 200;
+window.dddyThresh = .01;
 
 function preload() {
-
-    game.load.image('mushroom', '/images/mushroom2.png');
-    game.load.spritesheet('chain','/images/chain.png',16,26);
-
+  game.load.image('mushroom', '/images/mushroom2.png');
 }
 
 function create() {
+  game.input.recordPointerHistory = true;
+  game.input.recordLimit = 100;
+  game.input.recordRate = 20;
 
-	//	Enable p2 physics
-	game.physics.startSystem(Phaser.Physics.P2JS);
-
-  //  Add 2 sprites which we'll join with a spring
   sprite1 = game.add.sprite(200, 300, 'mushroom');
+  sprite2 = game.add.sprite(100, 300, 'mushroom');
 
-	game.physics.p2.enable(sprite1);
-  sprite1.body.kinematic = true;
-
-  springTop = game.add.sprite(200, 40, 'chain', 1);
-  springBall = game.add.sprite(200, dropTop, 'mushroom');
-	game.physics.p2.enable([springTop, springBall]);
-  //  The parameters are: createSpring(sprite1, sprite2, restLength, stiffness, damping, worldA, worldB, localA, localB)
-  game.physics.p2.createSpring(springTop, springBall, 120, 25, 1);
-  springTop.body.kinematic = true;
-  springBall.body.data.fixedRotation = true;
-
-  game.input.onDown.add(function() {
-    isMoving = true;
-    accelerateToObject(springBall,target);
-  }, this);
-  game.input.onUp.add(function() {
-    isMoving = false;
-  }, this);
-  game.input.addMoveCallback(move, this);
-
-  game.physics.p2.gravity.y = 0;
-
-  springBall.body.onBeginContact.add(function blockHit (body, shapeA, shapeB, equation) {
-    if (body === sprite1.body) {
-      console.log("whammo");
-      metronome.playNote();
-      springBall.body.y -= 50;
-      springBall.body.force.y = 0;
-      springBall.body.velocity.y = 0;
-      throttleMoving = true;
-    }
-  }, this);
+  game.input.addMoveCallback(onMove);
 }
 
 function update() {
-  springBall.body.x = 200;
 }
 
-
-function accelerateToObject(obj1, obj2, speed) {
-  if (typeof speed === 'undefined') { speed = .5*1e5; }
-  var angle = Math.atan2(obj2.y - obj1.y, obj2.x - obj1.x);
-  //obj1.body.force.x = Math.cos(angle) * speed;
-  //obj1.body.force.y = Math.sin(angle) * speed;
-  obj1.body.force.y = speed;
+function render() {
 }
 
-function move(pointer, x, y) {
-  if (isMoving){
-    if (nextMoveTime > game.time.now) {
-      return;
+function onMove(pointer) {
+  var dt = game.time.now - oldTime;
+  oldTime = game.time.now;
+  if (dt > timeThresh) {
+    oldSign = 0;
+  }
+
+  ptrMotionState = getPointerMotionState(pointer);
+  if (ptrMotionState) {
+    sprite1.height = 50 * ptrMotionState.ddy;
+    var newSign;
+    //newSign = (ptrMotionState.ddy < 0) ? -1 : 1;
+    if (Math.abs(ptrMotionState.dddy) > dddyThresh) {
+      newSign = (ptrMotionState.dddy < 0) ? -1 : 1;
+    } else {
+      newSign = oldSign;
     }
-    nextMoveTime = game.time.now + moveDelay;
-
-    target.setTo(x, y);
-    accelerateToObject(springBall,target);
+    if (oldSign != newSign) {
+      sprite2.height *= -1;
+      metronome.playNote();
+    }
+    oldSign = newSign;
   }
 }
 
+function getPointerMotionState(pointer, lookbackTime) {
+  lookbackTime = lookbackTime || window.lookbackTime || 100;
 
-function render() {
+  var motionState = {};
+
+  var lookbackPoints = Math.floor(lookbackTime/game.input.recordRate);
+  var alignedLookbackTime = lookbackPoints * game.input.recordRate;
+  var lookbackIdx = pointer._history.length - 1 - lookbackPoints;
+  var startIdx = Math.max(0, lookbackIdx);
+  var start = pointer._history[startIdx];
+  var end = pointer._history[pointer._history.length - 1];
+
+  if (! start || ! end){
+    return;
+  }
+
+  motionState.dx = end.x - start.x;
+  motionState.dy = end.y - start.y;
+
+  motionState.ddx = motionState.dx/alignedLookbackTime;
+  motionState.ddy = motionState.dy/alignedLookbackTime;
+
+  motionState.dddx = 0;
+  motionState.dddy = 0;
+
+  for (var i=startIdx; i < pointer._history.length - 1; i++) {
+    var a = pointer._history[i];
+    var b = pointer._history[i+1];
+    motionState.dddx += (b.x - a.x)/(Math.pow(game.input.recordRate,2));
+    motionState.dddy += (b.y - a.y)/(Math.pow(game.input.recordRate,2));
+  }
+
+  return motionState;
 
 }
