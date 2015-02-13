88c5a500a62e5df5cf44b465abc6484252d7da2b
adorsk <adorsk@gmail.com>
2015-02-13 14:28:03 -0500

    w/out collision groups

--- app/js/brush.js
+++ app/js/brush.js
@@ -7,9 +7,6 @@ var game = new Phaser.Game(400, 400, Phaser.AUTO, 'phaser-example', { preload: p
 var brush;
 var springNode;
 
-// Collision groups.
-var cg = {};
-
 function preload() {
   game.stage.backgroundColor = '#DDDDDD';
 
@@ -22,24 +19,17 @@ function create() {
 
   // Initialize physics.
 	game.physics.startSystem(Phaser.Physics.P2JS);
-  game.physics.p2.setImpactEvents(true);
-  game.physics.p2.updateBoundsCollisionGroup();
-
-  // Set up collision group for brush.
-  cg.brush = game.physics.p2.createCollisionGroup();
 
   // Setup spring node.
   springNode = game.add.sprite(200, 45);
   game.physics.p2.enable(springNode, true, true);
   springNode.body.setRectangle(10,10);
   springNode.body.static = true;
-  springNode.body.setCollisionGroup(cg.brush);
 
   // Setup brush
   brush = game.add.sprite(200, 100, 'brush');
   brush.name = 'brush';
 	game.physics.p2.enable(brush);
-  brush.body.setCollisionGroup(cg.brush);
 
   // Setup brush physics.
   var spring = game.physics.p2.createSpring(springNode, brush, 50, 10, 1, null, null, [0,5],[0,0]);
@@ -47,22 +37,15 @@ function create() {
   pConstraint.upperLimitEnabled = true;
   pConstraint.upperLimit = -0.1;
 
-  // Setup scroll collision group.
-  cg.scroll = game.physics.p2.createCollisionGroup();
-
   // Setup scroll.
   scroll = game.add.sprite(200, 300, 'scroll');
-  scroll.name = 'scroll';
 	game.physics.p2.enable(scroll);
-  scroll.body.setCollisionGroup(cg.scroll);
+  scroll.name = 'scroll';
 
   // Setup scroll physics.
   scroll.body.kinematic = true;
 
   // Handle brush-scroll collision.
-  scroll.body.collides(cg.brush);
-  //brush.body.collides(cg.scroll, hitScroll, this);
-  brush.body.collides(cg.scroll);
   scroll.body.onBeginContact.add(onScrollContact);
 
   game.input.onDown.add(onDown);
@@ -78,10 +61,6 @@ function onDown(){
 }
 
 function render() {
-}
-
-function hitScroll(bodyA, bodyB) {
-  console.log('hs', bodyA.data.id, bodyB.data.id);
 }
 
 function onScrollContact() {
