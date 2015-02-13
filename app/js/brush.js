ef8f7611c8fac3396a043fa4d3ee282953df78b2
adorsk <adorsk@gmail.com>
2015-02-13 14:47:30 -0500

    draw two brush sets

--- app/js/brush.js
+++ app/js/brush.js
@@ -4,8 +4,6 @@ var metronome = require('./metronome');
 console.log('yo');
 
 var game = new Phaser.Game(400, 400, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
-var brush;
-var springNode;
 
 function preload() {
   game.stage.backgroundColor = '#DDDDDD';
@@ -20,33 +18,8 @@ function create() {
   // Initialize physics.
 	game.physics.startSystem(Phaser.Physics.P2JS);
 
-  // Setup spring node.
-  springNode = game.add.sprite(200, 45);
-  game.physics.p2.enable(springNode, true, true);
-  springNode.body.setRectangle(10,10);
-  springNode.body.static = true;
-
-  // Setup brush
-  brush = game.add.sprite(200, 100, 'brush');
-  brush.name = 'brush';
-	game.physics.p2.enable(brush);
-
-  // Setup brush physics.
-  var spring = game.physics.p2.createSpring(springNode, brush, 50, 10, 1, null, null, [0,5],[0,0]);
-  var pConstraint = game.physics.p2.createPrismaticConstraint(springNode, brush, true,[0,5],[0,-50],[0,1]);
-  pConstraint.upperLimitEnabled = true;
-  pConstraint.upperLimit = -0.1;
-
-  // Setup scroll.
-  scroll = game.add.sprite(200, 300, 'scroll');
-	game.physics.p2.enable(scroll);
-  scroll.name = 'scroll';
-
-  // Setup scroll physics.
-  scroll.body.kinematic = true;
-
-  // Handle brush-scroll collision.
-  scroll.body.onBeginContact.add(onScrollContact);
+  game.teacherSprites = addScrollAndBrush({x: 100});
+  game.playerSprites = addScrollAndBrush({x: 300});
 
   game.input.onDown.add(onDown);
 }
@@ -54,17 +27,59 @@ function create() {
 function update() {
 }
 
-
-function onDown(){
-  console.log('onDown');
-  brush.body.force.y = 1e5;
+function render() {
 }
 
-function render() {
+function addScrollAndBrush(opts) {
+  opts = opts || {};
+
+  var x = opts.x;
+  var springY = opts.springY || 50;
+  var scrollY = 300;
+  var springLen = opts.springLen || 50;
+  var springK = opts.springK || 10;
+  var damping = opts.damping || 1;
+
+  // Setup spring node.
+  var springNode = game.add.sprite(x, springY);
+  game.physics.p2.enable(springNode, true, true);
+  springNode.body.static = true;
+
+  // Setup brush
+  var brush = game.add.sprite(x, springY, 'brush');
+	game.physics.p2.enable(brush);
+
+  // Setup scroll.
+  var scroll = game.add.sprite(x, scrollY, 'scroll');
+	game.physics.p2.enable(scroll);
+
+  // Setup brush physics.
+  var springNodeAnchor = [0,0];
+  var brushAnchor = [0, -1 * brush.height/2.0];
+  var spring = game.physics.p2.createSpring(springNode, brush, springLen, springK, damping, null, null, springNodeAnchor, brushAnchor);
+  var pConstraint = game.physics.p2.createPrismaticConstraint(springNode, brush, true, springNodeAnchor, brushAnchor,[0,1]);
+  pConstraint.upperLimitEnabled = true;
+  pConstraint.upperLimit = -0.1;
+
+  // Setup scroll physics.
+  scroll.body.kinematic = true;
+
+  // Handle brush-scroll collision.
+  scroll.body.onBeginContact.add(onScrollContact);
+
+  return {
+    brush: brush,
+    scroll: scroll
+  };
 }
 
 function onScrollContact() {
   console.log('osc');
   metronome.playNote();
+}
+
+function onDown() {
+  var playerBrush = game.playerSprites.brush;
+  playerBrush.body.force.y = 1e5;
 }
 
