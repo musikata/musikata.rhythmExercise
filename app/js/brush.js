368904bb8dc4c95338a500386aad9c71178bd6c5
A. Dorsk <adorsk@gmail.com>
2015-02-15 20:54:51 -0500

    better bouncing beats

--- app/js/brush.js
+++ app/js/brush.js
@@ -9,71 +9,177 @@ window.addEventListener("load", function() {
   window.game = new Phaser.Game(400, 400, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 });
 
-var beatEvents = [0, 1, 2, 3];
-var beatLength = .05;
+var totalBeats = 4;
+var defaultNotes = [0, 1, 2, 2.5, 3];
+var noteEvents = [];
 var bpm = 60;
 var spb = 60/bpm;
+var defaultNoteLen = 1/32;
 var audioEvents = {};
 var audioEventWindow = spb/2.0;
+var audioDelay = 1;
+var isBouncing = false;
+var bounceHeight = 30;
+var bounceParameters = {};
 
-window.beatEventsToAudioEvents = function() {
-  var audioNow = audioCtx.currentTime;
-  beatEvents.forEach(function(beatEvent) {
-    var id;
-    var tag;
-    var start = audioNow + (spb * beatEvent);
-    var end = start + beatLength;
 
-    // Start event.
-    tag = 'beat.start';
-    id = tag + ':' + start;
-    audioEvents[id] = {
-      id: id,
-      tags: [tag],
-      time: start,
-    };
 
-    // End event.
-    tag = 'beat.end';
-    id = tag + ':' + end;
-    audioEvents[id] = {
-      id: id,
-      tags: [tag],
-      time: end,
-    };
+window.notesToAudioEvents = function(notes) {
+
+  // Format note as object if provided as number.
+  for (var i=0; i < notes.length; i++) {
+    var note = notes[i];
+    if (typeof(note) == 'number') {
+      note = notes[i] = {start: note}
+    }
+
+    // Set note end if not provided.
+    if (! note.end) {
+      note.end = note.start + defaultNoteLen;
+    }
+  }
+
+  notesToNoteEvents(notes);
+  noteEventsToAudioEvents();
+}
+
+window.noteEventsToAudioEvents = function() {
+  var audioNow = audioCtx.currentTime + audioDelay;
+
+  noteEvents.forEach(function(noteEvent) {
+    var audioT = audioNow + (spb * noteEvent.t);
+
+    var evtData = {noteEvent: noteEvent};
+
+    var isNoteEvent = noteEvent.tags && (
+      (noteEvent.tags.indexOf('note.start') > -1) ||
+      (noteEvent.tags.indexOf('note.end') > -1));
+    if (isNoteEvent) {
+      //TODO:  schedule audio here.
+    }
+
+    var isBounceEvent = noteEvent.tags && (
+      noteEvent.tags.indexOf('bounce.start') > -1);
+    if (isBounceEvent) {
+      var bounceParameters = {
+        start: audioNow + (spb * noteEvent.bounce.start),
+        end: audioNow + (spb * noteEvent.bounce.end)
+      };
+      bounceParameters.radius = (bounceParameters.end - bounceParameters.start)/2.0;
+      bounceParameters.middle = bounceParameters.start + bounceParameters.radius;
+      evtData.bounceParameters = bounceParameters;
+    }
+
+    if (! audioEvents[audioT]) {
+      audioEvents[audioT] = [];
+    }
+    audioEvents[audioT].push({
+      tags: noteEvent.tags,
+      t: audioT,
+      data: evtData
+    });
+  });
+}
+
+window.notesToNoteEvents = function(notes) {
+  if (notes.length < 1) {
+    return;
+  }
+
+  noteEvents = [];
+  var firstNote = notes[0];
+  var lastNote = notes[notes.length - 1];
+
+  // Start bouncing at beat 0.
+  noteEvents.push({
+    tags: ['bouncing.toggle'],
+    t: 0
+  });
+
+  // Initial bounce.
+  var firstBounceStart = lastNote.end - totalBeats;
+  noteEvents.push({
+    tags: ['bounce.start'],
+    t: firstBounceStart,
+    bounce: {
+      start: firstBounceStart,
+      end: firstNote.start
+    }
+  });
+
+  // Notes.
+  for (var i=0; i < notes.length-1; i++) {
+    var currentNote = notes[i];
+    var nextNote = notes[i+1];
+
+    noteEvents.push({
+      tags: ['note.start'],
+      t: currentNote.start,
+    });
+
+    noteEvents.push({
+      tags: ['note.end'],
+      t: currentNote.end
+    });
+
+    noteEvents.push({
+      tags: ['bounce.start'],
+      t: currentNote.end,
+      bounce: {
+        start: currentNote.end,
+        end: nextNote.start
+      }
+    });
+  }
+  
+  // Ending bounce.
+  noteEvents.push({
+    tags: ['bounce.start'],
+    t: lastNote.end,
+    bounce: {
+      start: lastNote.end,
+      end: firstNote.start + totalBeats
+    }
+  });
+
+  // Stop bouncing.
+  noteEvents.push({
+    tags: ['bouncing.toggle'],
+    t: totalBeats
   });
 }
 
 // Process collection of audio events.
+// This is run on every update loop.
 function processAudioEvents() {
+  if (audioEvents.length < 1 ){
+    return;
+  }
+
   var audioNow = audioCtx.currentTime;
 
-  for (var id in audioEvents) {
-    var evt = audioEvents[id];
-    var dt = evt.time - audioNow;
-    if (Math.abs(dt) < audioEventWindow){
-      processAudioEvent(evt, audioNow, dt);
-    } else if (dt < (-1 * audioEventWindow)) {
-      console.log("removing ", id);
-      processAudioEvent(evt, audioNow, dt, true);
-      delete audioEvents[id];
-    }
-  };
+  for (var audioT in audioEvents) {
+    var isActive = (audioT <= audioNow);
+    if (isActive) {
+      var evts = audioEvents[audioT];
+      evts.forEach(function(evt) {
+        processAudioEvent(evt, audioNow);
+      });
+      console.log("removing ", audioT);
+      delete audioEvents[audioT];
+    };
+  }
 }
 
-// Process a single audio event.
-function processAudioEvent(evt, audioNow, dt, removing) {
+function processAudioEvent(evt, audioNow) {
+  console.log('processing: ', evt);
   evt.tags.forEach(function(tag) {
-    if (removing) {
-      tag += '.removing';
-    }
     postal.publish({
       channel: 'audio',
       topic: tag,
       data: {
         audioNow: audioNow,
-        dt: dt,
-        evt: evt
+        evtData: evt.data
       }
     });
   });
@@ -101,36 +207,53 @@ function create() {
   var brushStartY = game.teacherSprites.brush.body.y;
   var brushEndY = game.teacherSprites.scroll.body.y - game.teacherSprites.brush.height/2;
   var brushYRange = brushEndY - brushStartY;
-  var brushSub = postal.subscribe({
+  var bouncingSub = postal.subscribe({
     channel: "audio",
-    topic: "beat.start",
+    topic: "bouncing.toggle",
     callback: function(data, envelope) {
-      // Parameterize t in terms of audioEventWindow (-1 to 1)
-      var t = data.dt/audioEventWindow;
-
-      // Update brush pos based on parameterized t.
-      //var yOffset = brushYRange * Math.cos(Math.PI/2 * Math.abs(t));
-      var yOffset = brushYRange * (Math.pow(1-Math.abs(t),2));
-      game.teacherSprites.brush.body.y = brushStartY + yOffset;
+      console.log('on bouncing.toggle');
+      isBouncing = ! isBouncing;
     }
   });
 
-  var brushSubRemoving = postal.subscribe({
+  var bounceSub = postal.subscribe({
     channel: "audio",
-    topic: "beat.start.removing",
+    topic: "bounce.start",
     callback: function(data, envelope) {
-      game.teacherSprites.brush.body.y = brushStartY;
+      console.log('on bouncing.start');
+      bounceParameters = data.evtData.bounceParameters;
+      bounceParameters.height = bounceHeight * (2*bounceParameters.radius)/spb;
     }
   });
 
   // Setup audio events.
-  beatEventsToAudioEvents();
+  notesToAudioEvents(defaultNotes);
+  console.log('ae: ', audioEvents);
 }
 
 function update() {
   processAudioEvents();
+
+  // Update teacher brush if bouncing.
+  if (isBouncing) {
+    var audioT = audioCtx.currentTime;
+
+    // Parameterize time in terms of current bounce parameters.
+    var t = (audioT - bounceParameters.middle)/bounceParameters.radius;
+    t = Math.max(-1, t);
+    t = Math.min(1, t);
+
+    // Update brush pos based on parameterized t.
+    var yOffset = bounceParameters.height * (1 - Math.pow(t,2));
+    if (yOffset && t) {
+      var scrollTop = game.teacherSprites.scroll.body.y - game.teacherSprites.scroll.height/2;
+      game.teacherSprites.brush.body.y = scrollTop - yOffset - game.teacherSprites.brush.height/2;
+    }
+  }
+
   game.teacherSprites.brush.body.force.y = 0;
   game.teacherSprites.brush.body.velocity.y = 0;
+
 }
 
 function render() {
@@ -196,6 +319,6 @@ function onDown() {
   var playerBrush = game.playerSprites.brush;
   playerBrush.body.force.y = 1e5;
   */
-beatEventsToAudioEvents();
+  notesToAudioEvents(defaultNotes);
 }
 
