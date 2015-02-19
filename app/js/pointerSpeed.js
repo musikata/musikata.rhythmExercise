eecde56990096ed97d511d8b5c13f16b3bcf9749
adorsk <adorsk@gmail.com>
2015-02-19 15:41:51 -0500

    nonlinear scaling for height, based on distance from droppoint

--- app/js/pointerSpeed.js
+++ app/js/pointerSpeed.js
@@ -4,8 +4,8 @@ metronome = require('./metronome');
 var gridConfig = {
   numCols: 12,
   numRows: 8,
-  width: 450,
-  height: 300,
+  width: 400,
+  height: 400,
 }
 gridConfig.cellWidth = gridConfig.width/gridConfig.numCols;
 gridConfig.cellHeight = gridConfig.height/gridConfig.numRows;
@@ -37,90 +37,39 @@ function preload() {
 
 
 function create() {
-  createLayout();
 	game.physics.startSystem(Phaser.Physics.P2JS);
 
+  /*
   ptrBody = game.add.sprite(0,0, 'mushroom');
   game.physics.enable(ptrBody, Phaser.Physics.P2JS);
   ptrBody.body.static = true;
-
-  /*
-  shroom = game.add.sprite(0,0, 'mushroom');
-  game.physics.enable(shroom, Phaser.Physics.P2JS);
-  shroom.body.kinematic = true;
-  shroom.body.fixedRotation = true;
-
   */
 
+  shroom = game.add.sprite(200,200, 'mushroom');
+  shroom.height = shroom.width = 25;
+
 }
-
-function createLayout() {
-  // Draw gutters.
-  renderGutter({
-    left: gridConfig.width - layoutConfig.gutterWidth
-  });
-  // Draw main area.
-}
-
-function renderGutter(opts) {
-  createSwipeArea({
-    x: 0,
-    y: 0,
-    height: layoutConfig.swipeAreaHeight,
-    width: layoutConfig.gutterWidth,
-    borderWidth: layoutConfig.gutterBorderWidth
-  });
-
-  createClickArea({
-    x: 0,
-    y: layoutConfig.swipeAreaHeight,
-    height: layoutConfig.clickAreaHeight,
-    width: layoutConfig.gutterWidth,
-    borderWidth: layoutConfig.gutterBorderWidth
-  });
-}
-
-function createSwipeArea(opts) {
-  var g = game.add.graphics(opts.x, opts.y);
-  var borderWidth = opts.borderWidth;
-  g.lineStyle(borderWidth, 0xccccccc, 1);
-  g.beginFill(0xffffff);
-  g.drawRect(borderWidth/2, borderWidth/2, opts.width, opts.height);
-  g.endFill(0xff3300);
-}
-
-function createClickArea(opts) {
-  var g = game.add.graphics(opts.x, opts.y);
-  var borderWidth = opts.borderWidth;
-  g.lineStyle(borderWidth, 0xccccccc, 1);
-  g.beginFill(0xffffff);
-  g.drawRect(borderWidth/2, borderWidth/2, opts.width, opts.height);
-  g.endFill(0xff3300);
-}
-
-var playingCounter = 0;
 function update() {
   var ptr = game.input.activePointer;
-
-  ptrBody.body.x = ptr.x;
-  ptrBody.body.y = ptr.y;
-
-  var minSpeed = 0;
-  var maxSpeed = 25;
-  var minSize = 75;
-  var maxSize = 150;
-  var speed = Math.abs(game.input.speed.y);
-  var scale = Math.min(1, (speed - minSpeed)/(maxSpeed - minSpeed));
-  scale = scale * scale;
-  var size = minSize + (scale * (maxSize - minSize));
-  ptrBody.width = ptrBody.height = size;
-  if (scale == 1 && playingCounter <= 0) {
-    metronome.playNote();
-    playingCounter = 5;
+  if (ptr.isDown) {
+    updateSpeedometer(ptr);
   }
-  playingCounter--;
-
 }
+
+window.base = 1.5;
+window.maxDelta = 75;
+function updateSpeedometer(ptr) {
+  var delta = ptr.position.y - ptr.positionDown.y;
+  var sign = delta < 0 ? -1 : 1;
+  var maxSize = 200;
+  var normalizedDelta = Math.min(1, Math.abs(delta)/maxDelta);
+  var scale = (Math.pow(base, normalizedDelta) - 1)/(base - 1);
+  //var scale = Math.pow(normalizedDelta, 2);
+  var size = sign * scale * maxSize;
+
+  shroom.height = size;
+}
+
 
 function render() {
 }
