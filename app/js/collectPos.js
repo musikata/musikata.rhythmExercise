5d570cb7585aac33d183681cdd0bc48bcb2068e6
adorsk <adorsk@gmail.com>
2015-02-25 12:29:26 -0500

    more testing on touch interface

---
+++ app/js/collectPos.js
@@ -0,0 +1,23 @@
+Phaser = require('Phaser');
+
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { create: create, update: update });
+
+function preload() {
+}
+
+window.data = [];
+
+function create() {
+}
+
+function update() {
+  var ptr = game.input.activePointer;
+  if (ptr.isDown) {
+    data.push({
+      t: game.time.time,
+      y: ptr.y,
+      yDown: ptr.positionDown.y,
+      timeDown: ptr.timeDown,
+    });
+  }
+}
